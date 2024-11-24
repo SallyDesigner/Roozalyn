072dde547add4789564ab171516a1805fead1d8c
SallyDesigner <sally28webdesigner@gmail.com>
2024-11-24 19:51:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-sider").onclick = () => {
+    document.getElementById("sider").classList.toggle("show");
+    document.getElementById("btn-sider").classList.toggle("clicked");
+}
